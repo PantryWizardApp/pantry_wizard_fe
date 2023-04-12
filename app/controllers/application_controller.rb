291e383f7a9c3284e328dbea6951b771e8cc88eb
Caleb Thomas <cjthomas00@comcast.net>
2023-04-12 11:46:45 -0600

    add #current_user, and #validate_user to application_controller. fix spacing on views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :validate_user, only: %i[new show]
+  helper_method :current_user
+
+  def current_user
+    @_current_user ||= User.find(session[:user]) if session[:user]
+  end
+
+  def validate_user
+    return if current_user
+
+    flash[:error] = "You must be logged in to access this page"
+    redirect_to root_path
+  end
 end
