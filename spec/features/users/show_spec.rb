4d5ba247c5b3269bdf18d3bfdefa7390969c8b50
Dawson Timmons <DawsonTimmons@gmail.com>
2023-04-16 15:53:47 -0600

    fix tests regarding google OAuth response changing

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -14,13 +14,23 @@ describe "User dashboard page" do
   end
 
   it "will allow an authorized user to visit the page" do 
-    @user = User.new({data:{attributes:{email: "bh1200@gmail.com", name: "Bob", google_id: "1234567890a"}}})
-    stub_request(:get, "http://localhost:5000/api/v1/users/#{@user.google_id}")
-      .to_return(status: 200, body: @json_response)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    current_user = {"google_id"=>"106048413854356122337",
+    "name"=>"Conner Van Loan",
+    "email"=>"cdvanloan@gmail.com",
+    "intolerances"=>nil,
+    "likes"=>nil,
+    "dislikes"=>nil,
+    "dietary_restrictions"=>nil}
 
-    visit "/dashboard"
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
 
-    expect(current_path).to eq("/dashboard")
+    data = {:data=>{:id=>"5", :type=>"user", :attributes=>{:email=>"cdvanloan@gmail.com", :name=>"Conner Van Loans", :google_id=>"106048413854356122337", :id=>5, :intolerances=>nil, :likes=>nil, :dislikes=>nil, :dietary_restrictions=>nil}}}
+    user = User.new(data)
+    allow(UserFacade).to receive(:find_user_by_google_id).with(current_user["google_id"]).and_return(user)
+
+    VCR.use_cassette('user_dashboard') do
+      visit '/dashboard'
+      expect(current_path).to eq('/dashboard')
+    end
   end
 end
