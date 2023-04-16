885732eaa392a9c648326fc130dabf13441e11e9
Dawson Timmons <DawsonTimmons@gmail.com>
2023-04-16 14:14:00 -0600

    fix google oauth bug request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
   def show
-    @user = UserFacade.find_user_by_google_id(current_user.google_id)
+    @user = UserFacade.find_user_by_google_id(current_user["google_id"])
   end
 end
