949d37e638390d4e02096ce2a713f60412a704bd
Caleb Thomas <cjthomas00@comcast.net>
2023-04-18 10:48:12 -0600

    update localhost to 4000.

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,31 +1,34 @@
 class RecipeService
-  def self.conn
-    Faraday.new(url:"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com") do |faraday|
-      faraday.headers["X-RapidAPI-Key"] = ENV["X-RapidAPI-Key"]
-    end
-  end
-
+  
   def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
-
+  
   def self.search_recipes(params)
     get_url("/recipes/search?#{params}&number=5")
   end
-
+  
   def self.get_recipe(id)
     get_url("/recipes/#{id}/information")
   end
-
+  
   def self.get_preferred_recipes(user)
     params = {
-        cuisine: user.likes.join(","),
-        excludeCuisine: user.dislikes.join(","),
-        intolerances: user.intolerances.join(","),
-        diet: user.dietary_restrictions
-      }
+    cuisine: user.likes.join(","),
+    excludeCuisine: user.dislikes.join(","),
+    intolerances: user.intolerances.join(","),
+    diet: user.dietary_restrictions
+  }
+  
+  get_url("/recipes/complexSearch?#{params}&number=5")
+end
 
-      get_url("/recipes/complexSearch?#{params}&number=5")
+  private 
+  
+  def self.conn
+    Faraday.new(url:"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com") do |faraday|
+      faraday.headers["X-RapidAPI-Key"] = ENV["X-RapidAPI-Key"]
+    end
   end
 end
