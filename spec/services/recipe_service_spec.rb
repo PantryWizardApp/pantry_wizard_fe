c69777a76177f79a3e3d8aec8838ebcb57887338
Caleb Thomas <cjthomas00@comcast.net>
2023-04-18 10:44:38 -0600

    add vcr to tests. removes extra spacing and pry's. update recipe show tests.

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -4,40 +4,23 @@ RSpec.describe RecipeService do
   context "class methods" do
     context "search_recipes" do
       it "returns recipes based on search params" do
-        recipe = File.read("spec/fixtures/recipe.json")
-        stub_request(:get, "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=5&query=pasta").
-         with(
-           headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v2.7.4',
-          'X-Rapidapi-Key'=>ENV["X-RapidAPI-Key"]
-           }).
-         to_return(status: 200, body: recipe, headers: {})
-        
-        pasta = RecipeService.search_recipes("query=pasta&number=10")
-        expect(pasta).to be_a(Hash)
-        expect(pasta[:results].size).to eq(10)
-        expect(pasta.keys).to eq([:results, :baseUri, :offset, :number, :totalResults, :processingTimeMs, :expires])
+        VCR.use_cassette('search_recipes') do
+          pasta = RecipeService.search_recipes("query=pasta&number=5")
+          expect(pasta).to be_a(Hash)
+          expect(pasta[:results].size).to eq(5)
+          expect(pasta.keys).to eq([:results, :baseUri, :offset, :number, :totalResults, :processingTimeMs, :expires])
+        end
       end
     end
 
     context "get_recipe" do
       it "returns a recipe based on id" do
-        blt_pasta = File.read("spec/fixtures/blt_pasta.json")
-        stub_request(:get, "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/491786/information").
-        with(
-          headers: {
-         'Accept'=>'*/*',
-         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-         'User-Agent'=>'Faraday v2.7.4',
-         'X-Rapidapi-Key'=>ENV["X-RapidAPI-Key"]
-          }).
-        to_return(status: 200, body: blt_pasta, headers: {})
-        blt = RecipeService.get_recipe(491786)
-        expect(blt).to be_a(Hash)
-        expect(blt[:id]).to eq(491786)
-        expect(blt[:title]).to eq("Ranch BLT Pasta Salad")
+        VCR.use_cassette('search_recipe_id') do
+          blt = RecipeService.get_recipe(491786)
+          expect(blt).to be_a(Hash)
+          expect(blt[:id]).to eq(491786)
+          expect(blt[:title]).to eq("Ranch BLT Pasta Salad")
+        end
       end
     end
 
@@ -63,6 +46,8 @@ RSpec.describe RecipeService do
 
         VCR.use_cassette("get_preferred_recipes") do
           service = RecipeService.get_preferred_recipes(user)
+          expect(service).to be_a(Hash)
+          expect(service[:results].size).to eq(5)
         end
       end
     end
