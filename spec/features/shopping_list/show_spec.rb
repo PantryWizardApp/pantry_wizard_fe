b89266c64c4cde55c3ac9e67eee466fecaa934b3
Brian Hayes <Brian.hayes1200@gmail.com>
2023-04-19 16:28:18 -0600

    Delete test for shopping list show page

--- spec/features/shopping_list/show_spec.rb
+++ spec/features/shopping_list/show_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "shopping list show" do 
-  it "will have a map" do 
+# RSpec.describe "shopping list show" do 
+#   # it "will have a map" do 
 
-    @user = User.new({data:{attributes:{email: "bh1200@gmail.com", name: "Bob", google_id: "1234567890a"}}})
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+#   #   @user = User.new({data:{attributes:{email: "bh1200@gmail.com", name: "Bob", google_id: "1234567890a"}}})
+#   #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-    visit shopping_list_path
- save_and_open_page
-    expect(page).to have_content("Search for grocery stores near you")
-  end
-end
+#   #   visit shopping_list_path
+ 
+#   #   expect(page).to have_content("Search for grocery stores near you")
+#   end
+# end
