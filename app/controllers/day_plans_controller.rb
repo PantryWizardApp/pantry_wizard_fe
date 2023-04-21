ba7a7466ef1443be6b546a335ddaa19c46802612
Conner Van Loan <cdvanloan@gmail.com>
2023-04-21 10:32:58 -0600

    fixes app breaking when user selects date they have already selected. Also makes it so user cannot select a date in the past

--- app/controllers/day_plans_controller.rb
+++ app/controllers/day_plans_controller.rb
@@ -6,7 +6,12 @@ class DayPlansController < ApplicationController
   def create
     user = UserFacade.find_user_by_google_id(current_user["google_id"])
     day_plan = DayPlanFacade.create_day_plan_with_meals(user, {date: params[:date_of_meal_plan]})
-    redirect_to "/dashboard"
+    if day_plan[:errors]
+      redirect_to "/day_plans/new"
+      flash[:error] = "You already have a meal plan for that day. Please select a different date."
+    else
+      redirect_to "/dashboard"
+    end
   end
 
   def show
